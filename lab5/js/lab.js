b66e350146a927554f3fea92327f1d97c4135173
WY-OwO <jjv_2014@hotmail.com>
2022-10-16 22:13:10 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,19 @@
+/**
+*Author: Wei-Yan Chiang;
+*Created: October 15, 2022;
+*License: Public Domain;
+**/
+
+make = "Proton";
+model = "Wira";
+color = "Red";
+year = 1995;
+ownIt = false;
+
+age = 2022 - year;
+
+document.writeln("Make: "+ make +"<br>");
+document.writeln("Model: "+ model +"<br>");
+document.writeln("Color: "+ color +"<br>");
+document.writeln("Year: "+ year + "<br>");
+document.writeln("Age " + age + "years<br>");
